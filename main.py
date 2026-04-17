43277a9d335af3e938ae4d23ff538aa47ccf32a3
andreasdecarvalho-prog <andreasdecarvalho.prog@gmail.com.com>
2026-04-17 10:38:43 -0300

    products table logic done

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
-from subprocess import run
-
+from ETL.extract import extract_to_csv
+from ETL.silver.fakestore import transform_fakestore
+from ETL.silver.dummy import transform_dummy
+from ETL.silver.silver_logic import products_dfs_to_db
 
 def main():
-    run(["python3", "ETL/silver_logic.py"], check=True)
+
+    # get data
+    file_names = extract_to_csv()
+
+
+    # transform data
+    dfs = []
+    dfs.append(transform_dummy(file_names[0]))
+    dfs.append(transform_fakestore(file_names[1]))
+    
+    
+    # create silver_products db with already parsed data that is still dfs
+    products_dfs_to_db(file_names, dfs)
+
+
+    # generate fake sales data into sales table
+    ...
+
+
+    # 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 main()
